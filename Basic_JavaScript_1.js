c9b4accc3beffbec2a564221cca4612ffd0c4059
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-08-12 23:25:52 -0700

    Add files via upload
    First commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,17 @@
+window.alert("Hello, world!");
+
+var A = "This is a string";
+
+
+document.write('Hello, world!');
+document.write(A);
+
+document.write("Harry told Sally,\"I see the ocean, Sally!\"<br>\"This is your first time seeing the ocean?!\"Sally responded.");
+
+docuument.write("\"Be who you are and say what you feel," + " because those who don\'t matter and and those who matter don\'t mind.\"" + "-Dr. Seuss");
+
+var Family = "Borts", Dad = "Blek", Mom = "Slek", Daughter = "Plek", Son = "Jlek";//the family class
+var Son = Son.fontcolor("blue") //The sons color
+document.write(Son)//where i am pointing to.
+
+Document.write (10 + 10);//this should equal 20
