a58347c55213846f8e851c66f12be8a6525cde06
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-08-14 00:35:37 -0700

    Add files via upload
    2nd commit

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,17 +1,28 @@
-window.alert("Hello, world!");
+window.alert("Hello,World!");
+document.write('Hello,World!');
 
+var X = "Goodbye, World!";
 var A = "This is a string";
+var B = "\"Be who you are and say what you feel,"
++ " because those who don\'t matter and and those who matter don\'t mind.\""
++ "-Dr. Seuss";
+var C = "5";
+var T = "8";
+var Family = "Borts", Dad = "Blek", Mom = "Slek", Daughter = "Plek", Son = "Jlek";//The family class
+var Son = Son.fontcolor("blue"); //The sons color
 
 
-document.write('Hello, world!');
+document.write(X);
 document.write(A);
+
 
 document.write("Harry told Sally,\"I see the ocean, Sally!\"<br>\"This is your first time seeing the ocean?!\"Sally responded.");
 
-docuument.write("\"Be who you are and say what you feel," + " because those who don\'t matter and and those who matter don\'t mind.\"" + "-Dr. Seuss");
+document.write( X + B);//Concatenated + String
 
-var Family = "Borts", Dad = "Blek", Mom = "Slek", Daughter = "Plek", Son = "Jlek";//the family class
-var Son = Son.fontcolor("blue") //The sons color
-document.write(Son)//where i am pointing to.
+document.write(Son);//Where i am pointing to.
 
-Document.write (10 + 10);//this should equal 20
+Document.write (C + T);//This should equal 13
+
+
+
