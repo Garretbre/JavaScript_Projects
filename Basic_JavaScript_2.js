db17ec90e6fb540c27305146b0546847a0a92201
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-08-14 01:03:34 -0700

    Add files via upload
    First Commit

---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,7 @@
+window.alert("Don't click the button");
+
+function myFunction()  {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
