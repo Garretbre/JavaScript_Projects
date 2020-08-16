c898a672fc790802430eba30c7686020e1879240
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-08-15 23:49:58 -0700

    first commit

--- Basic_JavaScript_3.js
+++ Basic_JavaScript_3.js
@@ -2,5 +2,29 @@ alert("This is HTML3");
 
 function addition_Function() {
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML =`2 + 2=${addition}`;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+
+
+function subtraction_Function() {
+    var Subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+
+function multiplucation_Function() {
+    var simple_Math = 10 * 17;
+    document.getElementById("Math").innerHTML = "10 x 17 = " + simple_Math;
+}
+
+
+function division_Function() {
+    var simple_Math = 48 * 6;
+    document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+function more_Math () {
+    var simple_Math = (1 +2) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, 
+    divided in half and then subtracted by 5 equals " + simple_Math;
 }
