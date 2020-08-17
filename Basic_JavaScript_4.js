0569f21a2e761525cd8af40d29f996d457e7a99b
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-08-16 23:15:44 -0700

    first commit

---
+++ Basic_JavaScript_4.js
@@ -0,0 +1,25 @@
+function my_Dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Red",
+        Breed:"Shiba",
+        Age:7,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+
+}
+
+function my_Dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"Red",
+        Breed:"Shiba",
+        Age:7,
+        Sound:"Bark!"
+    };
+    delete Animal.Sound;
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+
+}
