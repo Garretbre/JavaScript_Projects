ed23a89db8f62b316b5bdb8600f849b9eee7f52f
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-08-26 11:57:56 -0700

    2nd commit

--- Basic_JavaScript_10.js
+++ Basic_JavaScript_10.js
@@ -1,60 +1,35 @@
-function count_To_Ten() {
+function counting_Ten() {
     var Digit = "";
     var X = 1;
     while (X < 11) {
         Digit += "<br>" + X;
         X++;
     }
-    document.getElementById("Counting_To_Ten").innerHTML = Digit;
+    document.getElementById("countingten").innerHTML = Digit;
 }
 
-var Instruments = ["Guitar", "Drums", "Piano", "Baas", "Violin", "Trumpet", "Flute"];
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content = "";
 var Y;
 function for_Loop() {
     for (Y = 0; Y < Instruments.length; Y++) {
         Content += Instruments[Y] + "<br>";
     }
-    document.getElementById("List_of_Instruments").innerHTML = Content;
+    document.getElementById("list of instruments").innerHTML = Content;
 }
 
-function constant_function() {
-    const Gaming_Console = {type:"game console", brand:"Playstation", color:"white"};
-    Gaming_Console.color = "black";
-    Gaming_Console.price = "$500";
-    document.getElementById("Constant").innerHTML = "The cost of the " +
-    Gaming_Console.type + " was " + Gaming_Console.price;
+function game_Price() {
+    const Gaming_Platform = {type:"console", brand:"playstation", color:"red"};
+    Gaming_Platform.color = "black";
+    Gaming_Platform.price = "$500";
+    document.getElementById("gaming").innerHTML = "The cost of the " +
+    Gaming_Platform.type + " was " + Gaming_Platform.price;
+
 }
 
-var X = 45;
-document.write(X);
-{
-    let X = 15;
-    document.write("<br> + X");
-}
-document.write("<br>" + X);
 
-var X = 45;
-document.write(X);
-{
-    var X = 15;
-    document.write("<br>" + X);
-}
-document.write("<br>" + X);
-
-function myFunction() {
-    {return Math.PI;
-}
-document.getElementById("demo").innerHTML = myFunction();
+function keyword_Deal() {
+  var carName = "Volvo";
+  document.getElementById("keyword").innerHTML = typeof carName + " " + carName;
 }
 
-let videogame = {
-    make = "Ps4",
-    model = "videogame",
-    year = "2020",
-    color = "blue",
-    description : function() {
-        return "The videogame is a " + this.year + this.color + this.make +this.model;
-    }
-};
-document.getElementById("Video_Object").innerHTML = videogame.description();
